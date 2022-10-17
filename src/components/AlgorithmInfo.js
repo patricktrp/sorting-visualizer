b9b9c53baa8b550a894dca2febef63697c617422
Patrick <57516425+patricktrp@users.noreply.github.com>
2022-10-18 00:02:12 +0200

    a

--- src/components/AlgorithmInfo.js
+++ src/components/AlgorithmInfo.js
@@ -1,43 +1,43 @@
 import { Box } from '@chakra-ui/react';
 
-const algorithmData = {
-    'bubbleSort': {
-        'name': 'Bubblesort',
-        'wc_time_complexity': 'O(N^2)',
-        'ac_time_complexity': 'O(N^2)',
-        'bc_time_complexity': 'O(N)'
-    },
-    'insertionSort': {
-        'name': 'Insertion Sort',
-        'wc_time_complexity': 'O(N^2)',
-        'ac_time_complexity': 'O(N^2)',
-        'bc_time_complexity': 'O(N)'
-    },
-    'selectionSort': {
-        'name': 'Selection Sort',
-        'wc_time_complexity': 'O(N^2)',
-        'ac_time_complexity': 'O(N^2)',
-        'bc_time_complexity': 'O(N^2)'
-    },
-    'mergeSort': {
-        'name': 'Mergesort',
-        'wc_time_complexity': 'O(N * log N)',
-        'ac_time_complexity': 'O(N * log N)',
-        'bc_time_complexity': 'O(N * log N)'
-    },
-    'quickSort': {
-        'name': 'Quicksort',
-        'wc_time_complexity': 'O(N^2)',
-        'ac_time_complexity': 'O(N * log N)',
-        'bc_time_complexity': 'O(N * log N)'
-    },
-    'heapSort': {
-        'name': 'Heapsort',
-        'wc_time_complexity': 'O(N * log N)',
-        'ac_time_complexity': 'O(N * log N)',
-        'bc_time_complexity': 'O(N * log N)'
-    },
-}
+// const algorithmData = {
+//     'bubbleSort': {
+//         'name': 'Bubblesort',
+//         'wc_time_complexity': 'O(N^2)',
+//         'ac_time_complexity': 'O(N^2)',
+//         'bc_time_complexity': 'O(N)'
+//     },
+//     'insertionSort': {
+//         'name': 'Insertion Sort',
+//         'wc_time_complexity': 'O(N^2)',
+//         'ac_time_complexity': 'O(N^2)',
+//         'bc_time_complexity': 'O(N)'
+//     },
+//     'selectionSort': {
+//         'name': 'Selection Sort',
+//         'wc_time_complexity': 'O(N^2)',
+//         'ac_time_complexity': 'O(N^2)',
+//         'bc_time_complexity': 'O(N^2)'
+//     },
+//     'mergeSort': {
+//         'name': 'Mergesort',
+//         'wc_time_complexity': 'O(N * log N)',
+//         'ac_time_complexity': 'O(N * log N)',
+//         'bc_time_complexity': 'O(N * log N)'
+//     },
+//     'quickSort': {
+//         'name': 'Quicksort',
+//         'wc_time_complexity': 'O(N^2)',
+//         'ac_time_complexity': 'O(N * log N)',
+//         'bc_time_complexity': 'O(N * log N)'
+//     },
+//     'heapSort': {
+//         'name': 'Heapsort',
+//         'wc_time_complexity': 'O(N * log N)',
+//         'ac_time_complexity': 'O(N * log N)',
+//         'bc_time_complexity': 'O(N * log N)'
+//     },
+// }
 
 const AlgorithmInfo = (props) => {
     return (
