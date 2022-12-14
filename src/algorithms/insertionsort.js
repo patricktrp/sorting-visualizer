c9a0eb1ffdb2175d6cbc6a73087f6bd14bc2cace
Patrick <57516425+patricktrp@users.noreply.github.com>
2022-12-14 18:00:54 +0100

    Update insertionsort.js

--- src/algorithms/insertionsort.js
+++ src/algorithms/insertionsort.js
@@ -1,5 +1,6 @@
 import colors from '../colors';
 import { swap } from '../utils';
+import { types } from '../animations';
 
 export const insertionSort = (arr) => {
     const animations = [];
@@ -9,40 +10,67 @@ export const insertionSort = (arr) => {
     for (let i = 1; i < arr.length; i++) {
         let j = i;
         animations.push({
-            type: 'COLOR',
-            indexes: [j],
-            color: 'cyan'
-        })
+            type: types.COLOR_RESET_BY_INDEX,
+            indexes: [i - 1]
+        });
+        animations.push({
+            type: types.COLOR,
+            indexes: [i],
+            color: colors.inorder
+        });
+
+        comparisons++;
         while (j > 0 && arr[j] < arr[j - 1]) {
             animations.push({
-                type: 'COLOR',
-                indexes: [j-1],
-                color: colors.comparing
+                type: types.COLOR,
+                indexes: [j, j - 1],
+                color: colors.primaryColorShade
             });
-            // animations.push({
-            //     type: 'COLOR',
-            //     indexes: [j, j - 1],
-            //     color: colors.red
-            // });
-            comparisons++;
-
             animations.push({
-                type: 'SWAP',
+                type: types.COLOR,
+                indexes: [j, j - 1],
+                color: colors.red
+            });
+            comparisons++;
+            swaps++;
+            animations.push({
+                type: types.SWAP,
                 indexes: [j, j - 1]
             });
-            animations.push({
-                type: 'COLOR',
-                indexes: [j, j - 1],
-                color: colors.green
-            });
-            animations.push({
-                type: 'COLOR_RESET_BY_INDEX',
-                indexes: [j, j-1]
-            });
-            swaps++;
+            // animations.push({
+            //     type: types.COLOR_RESET
+            // });
+
+            // TODO implement COLOR_MULTIPLE_AT_SAME_TIME type
+            if (j === i) {
+                animations.push({
+                    type: types.COLOR_RESET_BY_INDEX,
+                    indexes: [j - 1]
+                });
+                animations.push({
+                    type: types.COLOR,
+                    color: colors.inorder,
+                    indexes: [j]
+                })
+            } else {
+                animations.push({
+                    type: types.COLOR_RESET_BY_INDEX,
+                    indexes: [j, j - 1]
+                });
+            }
+
             swap(arr, j, j - 1);
             j--;
         }
+        animations.push({
+            type: types.COLOR,
+            indexes: [j],
+            color: colors.green
+        });
+        animations.push({
+            type: types.COLOR_RESET_BY_INDEX,
+            indexes: [j]
+        });
     }
 
     return [animations, comparisons, swaps];
